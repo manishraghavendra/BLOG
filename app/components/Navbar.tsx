a80a112cb4452b4e9c8d8ad5135098b13f9e06a4
manishraghavendra <www.manishrag@gmail.com>
2024-07-22 21:55:12 +0530

    final

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+import ThemeSwitch from "./ThemeSwitch";
+import { JetBrains_Mono } from "next/font/google";
+
+const font = JetBrains_Mono({ weight: "400", subsets: ["latin"] });
+
+const Navbar = () => {
+  return (
+    <div className="mx-auto max-w-5xl px-6">
+      <div className="flex justify-between items-center h-16 w-full">
+        <Link href="/">
+          <div className={`${font.className} text-2xl dark:text-amber-50`}>
+            Manish's
+            <span className="text-purple-500">Blog</span>
+          </div>
+        </Link>
+        <ThemeSwitch />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
